9fea127b174a625b8f72210580524db1e7ff81a7
samsondanicah <danicahsamson26@gmail.com>
2023-08-11 21:35:40 +0800

    adding and remove show, edit

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -4,4 +4,15 @@ class Users::ProfilesController < ApplicationController
   def edit
     @user = current_client_user
   end
+
+  def update
+    @user = current_client_user
+    if @user.update(user_params)
+      redirect_to profiles_path, notice: 'User was successfully updated.'
+    end
+  end
+
+  def user_params
+    params.permit(:phone_number, :username, :password)
+  end
 end
