718a78d4eb38c72e4480e221dd9ecbfae5c540d8
nicah <danicahsamson26@gmail.com>
2023-08-24 23:57:14 -0400

    feature 15 bet model

--- app/controllers/admin/bets_controller.rb
+++ app/controllers/admin/bets_controller.rb
@@ -1,6 +1,52 @@
 class Admin::BetsController < AdminController
+  before_action :set_bet, only: [:won, :lost, :cancel]
+
   def index
-    @bets = Bet.new
+    @bets = Bet.all
+
+    if params[:serial_number].present?
+      @bets = @bets.where(serial_number: params[:serial_number])
+    end
+
+    if params[:item_name].present?
+      @bets = @bets.joins(:item).where("items.name LIKE ?", "%#{params[:item_name]}%")
+    end
+
+    if params[:email].present?
+      @bets = @bets.joins(:user).where("users.email LIKE ?", "%#{params[:email]}%")
+    end
+
+    if params[:state].present?
+      @bets = @bets.where(state: params[:state])
+    end
+
+    if params[:start_date].present?
+      @bets = @bets.where("created_at >= ?", params[:start_date])
+    end
+
+    if params[:end_date].present?
+      @bets = @bets.where("created_at <= ?", params[:end_date])
+    end
   end
 
+  def win; end
+
+  def lose; end
+
+  def cancel
+    @bet = Bet.find(params[:bet_id])
+    if @bet.may_cancel?
+      flash[:notice] = 'Cancelled successfully'
+      @bet.cancel!
+    else
+      redirect_to bets_path, alert: 'Cancelled Bet'
+    end
+  end
+
+  private
+
+  def set_bet
+    @bet = Bet.find(params[:id])
+  end
 end
+
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,3 +1,43 @@
 class Bet < ApplicationRecord
+  include AASM
 
+  belongs_to :item
+  belongs_to :user
+
+  after_create :assign_serial_number
+  after_create :subtract_coin
+
+  aasm column: :state do
+    state :betting, initial: true
+    state :won, :lost, :cancelled
+
+    event :won do
+      transitions from: :betting, to: :won
+    end
+
+    event :lost do
+      transitions from: :betting, to: :lost
+    end
+
+    event :cancel do
+      transitions from: :betting, to: :cancelled, after: :refund_coin
+    end
+  end
+
+  private
+
+  def subtract_coin
+    user.update(coins: user.coins - 1)
+  end
+
+  def refund_coin
+    user.update(coins: user.coins + 1)
+  end
+
+  def assign_serial_number
+    number_count = format('%04d', item.bets.where(batch_count: item.batch_count).count)
+    date_format = Time.current.strftime('%y%m%d')
+    serial_number = "#{date_format}-#{item.id}-#{item.batch_count}-#{number_count}"
+    update(serial_number: serial_number)
+  end
 end
